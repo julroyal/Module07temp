b642631f81e85362039994bc94069a02515774b8
julroyal <julroyal5@gmail.com>
2023-10-16 19:47:00 -0400

    Initial commit

---
+++ src/partA.java
@@ -0,0 +1,17 @@
+public class partA {
+    public static void main(String[] args) {
+        int x = 0;
+         for(x = 0; x <= 30; x++) {
+            System.out.println(x);
+        }
+         for(x = 30; x >= 0; x--) {
+             System.out.println(x);
+         }
+         for(x = 0; x <= 18; x+=3) {
+             System.out.println(x);
+         }
+         for(x = 10; x >= 0; x-=2) {
+             System.out.println(x);
+         }
+    }
+}
